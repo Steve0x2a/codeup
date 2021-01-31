d02184bc3fab536b512ef9bff76a877cc9b82c49
Steve0x2a <stevesough@gmail.com>
2021-01-31 19:44:25 +0800

    2021-01-31

---
+++ 4/4.4 greedy/B1023.cpp
@@ -0,0 +1,20 @@
+#include <cstdio>
+int main(){
+    int count[10] = {0};
+    for(int i = 0; i < 10; i++){
+        scanf("%d",&count[i]);
+    }
+    for(int i = 1; i < 10; i++){
+        if(count[i]){
+            printf("%d",i);
+            count[i]--;
+            break;
+        } 
+    }
+    for(int i = 0; i < 10; i++){
+        while(count[i]--){
+            printf("%d",i);
+        }
+    }
+    return 0;
+}
